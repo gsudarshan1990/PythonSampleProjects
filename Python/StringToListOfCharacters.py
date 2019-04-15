d6ee5635df821beaa4e8185f5f267065a180ea55
Sudharshan Govindarajan <sudharshan.govindarajan@ey.com>
2019-04-15 09:58:41 +0530

    Second Commit

---
+++ Python/StringToListOfCharacters.py
@@ -0,0 +1,14 @@
+string='Parallel'
+my_set=set()
+for char in string:
+   my_set.add(char)
+
+print(my_set)
+
+
+string1 ='Mississippi'
+my_set=set()
+for char in string1:
+    my_set.add(char)
+
+print(my_set)
---
+++ Python/tuples.py
@@ -0,0 +1,13 @@
+tuple1=(1,2,3)
+my_list=[1,2,3]
+print(type(tuple1))
+print(type(my_list))
+print(len(tuple1))
+print(len(my_list))
+print(tuple1.index(3))
+tuple2=('a','a','a','b','a')
+print(tuple2.count('a'))
+
+
+my_list[0]='NEW'
+#tuple1[0]='NEW'
