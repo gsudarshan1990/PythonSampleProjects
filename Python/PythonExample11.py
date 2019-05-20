4fbc51b3544400b5b499626b650f248acf6e967b
Sudharshan Govindarajan <sudharshan.govindarajan@ey.com>
2019-05-20 16:19:20 +0530

    Commit Examples five

---
+++ Python/PythonExample11.py
@@ -0,0 +1,9 @@
+"""
+Exercise 3: Write a program to prompt the user for hours and rate per
+hour to compute gross pay.
+"""
+
+hours=input('Enter the hours you worked')
+rate=input('Enter the rate per hour')
+pay=(int(hours))*(float(rate))
+print(pay)
---
+++ Python/PythonExample13.py
@@ -0,0 +1,10 @@
+"""
+Write a program which prompts the user for a Celsius tem-
+perature, convert the temperature to Fahrenheit, and print out the
+converted temperature.
+"""
+
+ctemp=input('Enter the temperature in Celcius')
+farenheit=(int(ctemp)*(9/5))+32
+print('The farenheit temperature is',farenheit)
+
